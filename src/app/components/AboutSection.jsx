0901b8644a6c4363e4b35fc69309f9c51f020bac
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-15 21:27:50 +0300

    quick bugfix before deploy

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -62,7 +62,12 @@ const AboutSection = () => {
   return (
     <section id="about" className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src={"/images/aboutme.jpeg"} width={500} height={500} />
+        <Image
+          alt="about me"
+          src={"/images/aboutme.jpeg"}
+          width={500}
+          height={500}
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base text-justify lg:text-lg">
